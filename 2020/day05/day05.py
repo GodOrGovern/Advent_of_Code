fa6dc22896743c4435df8f1c1370316d73233fa1
David Keenan <willkeenan12@gmail.com>
2020-12-05 00:47:21 -0500

    Eliminate unnecessary parsing (each line is seat ID in binary)

--- 2020/day05/day05.py
+++ 2020/day05/day05.py
@@ -1,20 +1,14 @@
 def main():
-    max_id = 0
     ids = set()
+    val = {'F': 0, 'B': 1, 'L': 0, 'R': 1}
     with open('input') as data:
         for line in data:
-            pos = {'F': 0, 'B': 1, 'L': 0, 'R': 1}
-            row = sum((pos[n] << (6-i)) for i, n in enumerate(line[:7]))
-            col = sum((pos[n] << (2-i)) for i, n in enumerate(line[7:10]))
-            seat_id = row*8 + col
-            ids.add(seat_id)
-            max_id = max(seat_id, max_id)
-    print(max_id)
-    for row in range(128):
-        for col in range(8):
-            cur = row*8 + col
-            if cur not in ids and cur-1 in ids and cur+1 in ids:
-                print(cur)
+            ids.add(sum((val[n] << (9-i)) for i, n in enumerate(line[:-1])))
+    print(max(ids))
+    for cur in ids:
+        if cur+1 not in ids and cur+2 in ids:
+            print(cur+1)
+            break
 
 
 if __name__ == "__main__":
