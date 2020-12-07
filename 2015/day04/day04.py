1ea055eb9ddd10a313e6026c5b85f49fffc33430
David Keenan <willkeenan12@gmail.com>
2020-12-07 16:47:14 -0500

    Solution to day 4 of 2015 Advent of Code

---
+++ 2015/day04/day04.py
@@ -0,0 +1,14 @@
+from hashlib import md5
+
+def main():
+    data = 'yzbqklnj'
+    num1 = 0
+    while md5((data+str(num1)).encode()).hexdigest()[:5] != '00000':
+        num1 += 1
+    num2 = num1
+    while md5((data+str(num2)).encode()).hexdigest()[:6] != '000000':
+        num2 += 1
+    print(num1, num2)
+
+if __name__ == "__main__":
+    main()
