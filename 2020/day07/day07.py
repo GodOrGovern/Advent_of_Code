80b21e4b2476dc46e7b1195009fd5638b4f0720f
David Keenan <willkeenan12@gmail.com>
2020-12-07 13:50:53 -0500

    Shorten input parsing with regex

--- 2020/day07/day07.py
+++ 2020/day07/day07.py
@@ -1,22 +1,18 @@
+from re import match, findall
+
 def main():
     ''' Memoizing can_hold() and bags_held() is unnecessary '''
     bags = dict()
-    with open('input') as data:
-        for line in data:
-            line = line.strip().split()
-            bag = line[0] + line[1]
-            bags[bag] = dict()
-            if line[4] == 'no':
-                continue
-            for num, adj, color in zip(line[4::4], line[5::4], line[6::4]):
-                bags[bag][adj+color] = int(num)
+    for line in open('input'):
+        bag = match('\w+ \w+', line).group(0)
+        bags[bag] = {b: int(n) for n, b in findall('(\d) (\w+ \w+)', line)}
     def can_hold(bag):
         ''' Return True if 'bag' can hold 'shinygold' else False '''
-        return 'shinygold' in bags[bag] or any(can_hold(b) for b in bags[bag])
+        return 'shiny gold' in bags[bag] or any(can_hold(b) for b in bags[bag])
     def bags_held(bag):
         ''' Returns the number of bags held by 'bag' '''
         return sum(bags[bag][b]*(bags_held(b)+1) for b in bags[bag])
-    print(sum(map(can_hold, bags)), bags_held('shinygold'))
+    print(sum(map(can_hold, bags)), bags_held('shiny gold'))
 
 if __name__ == "__main__":
     main()
