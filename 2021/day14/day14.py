321627c9384790d2ce191374c8b86f5ded9d5f66
David Keenan <willkeenan12@gmail.com>
2021-12-14 20:03:50 -0500

    Simplify by using Counter

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter
 
 def main():
     polymer,rules = load_data()
@@ -16,28 +16,18 @@ def load_data():
         return polymer,rules
 
 def get_count_diff(polymer, rules, end):
-    c_to_n = {c:n for n,c in enumerate(set(polymer))}
-    chars = len(c_to_n)
     memoize = dict()
-    def expand(polymer, step):
-        if (polymer, step) in memoize:
-            return memoize[(polymer, step)]
-        count = [0] * chars
-        for c in polymer:
-            count[c_to_n[c]] += 1
-        if step != end:
-            for a,b in zip(polymer[:-1], polymer[1:]):
-                new = rules[a+b]
-                for n,x in enumerate(expand(a+new, step+1)):
-                    count[n] += x
-                for n,x in enumerate(expand(new+b, step+1)):
-                    count[n] += x
-                for c in (a, new, b):
-                    count[c_to_n[c]] -= 1
-        memoize[(polymer, step)] = tuple(count)
-        return memoize[(polymer, step)]
-    counts = expand(polymer, 0)
-    return max(counts) - min(counts)
+    def get_count(a, b, step):
+        if step == end:
+            return Counter('')
+        if (a, b, step) not in memoize:
+            new = rules[a+b]
+            memoize[(a,b,step)] = Counter(new) + get_count(a,new,step+1) + get_count(new,b,step+1)
+        return memoize[(a, b, step)]
+    counts = Counter(polymer)
+    for a,b in zip(polymer[:-1], polymer[1:]):
+        counts += get_count(a, b, 0)
+    return max(counts.values()) - min(counts.values())
 
 def part_one(polymer, rules):
     return get_count_diff(polymer, rules, 10)
